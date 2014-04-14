7da805c2d03df679d88333a14f79e27916d35bc8
tintinweb <tintinweb@oststrom.com>
2014-04-14 23:52:09 +0200

    utils: hexdump - initialize allnulls

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -2,6 +2,7 @@ def h2bin(x):
     return x.replace(' ', '').replace('\n', '').decode('hex')
 
 def hexdump_squashed(s):
+    allnulls = 0
     for b in xrange(0, len(s), 16):
         lin = [c for c in s[b : b + 16]]
         hxdat = ' '.join('%02X' % ord(c) for c in lin)
